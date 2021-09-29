0f3eb6f4805283ace898a7379c55dfc0db14debf
Thuy Quynh <ptq1202@gmail.com>
2021-09-29 08:54:34 +0700

    add swagger

--- src/main/java/com/scms/supplychainmanagementsystem/dto/RefreshTokenRequest.java
+++ src/main/java/com/scms/supplychainmanagementsystem/dto/RefreshTokenRequest.java
@@ -1,12 +1,14 @@
 package com.scms.supplychainmanagementsystem.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RefreshTokenRequest {
