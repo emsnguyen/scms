ef518d1a51d697a2b0e2f8b746d7c4722c87db13
Thuy Quynh <ptq1202@gmail.com>
2021-09-29 08:54:34 +0700

    add config to access swagger without authen

--- src/main/java/com/scms/supplychainmanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/scms/supplychainmanagementsystem/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -37,6 +38,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
 //        httpSecurity.addFilterBefore(jwtAuthenticationFilter,
 //                UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // Allow swagger to be accessed without authentication
+        web.ignoring().antMatchers("/v2/api-docs")//
+                .antMatchers("/swagger-resources/**")//
+                .antMatchers("/swagger-ui.html")//
+                .antMatchers("/configuration/**")//
+                .antMatchers("/webjars/**")//
+                .antMatchers("/public");
     }
 
     @Autowired
