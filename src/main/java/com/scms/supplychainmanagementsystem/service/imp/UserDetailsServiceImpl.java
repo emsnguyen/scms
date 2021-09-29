595d6eabaa901993f4aaec683aaed6017b55269c
Thuy Quynh <ptq1202@gmail.com>
2021-09-30 01:03:47 +0700

    add code service login

--- src/main/java/com/scms/supplychainmanagementsystem/service/imp/UserDetailsServiceImpl.java
+++ src/main/java/com/scms/supplychainmanagementsystem/service/imp/UserDetailsServiceImpl.java
@@ -1,22 +1,44 @@
 package com.scms.supplychainmanagementsystem.service.imp;
 
+import com.scms.supplychainmanagementsystem.entity.User;
 import com.scms.supplychainmanagementsystem.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collection;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+
 @Service
 @AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
-    // TODO: customize UserDetails
+    /**
+     * @param username
+     * @return
+     */
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) {
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("No user " +
+                "Found with username : " + username));
+
+        return new org.springframework.security
+                .core.userdetails.User(user.getUsername(), user.getPassword(),
+                user.isActive(), true, true,
+                true, getAuthorities(user.getRole().getRoleName()));
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String role) {
+        return singletonList(new SimpleGrantedAuthority(role));
     }
 }
