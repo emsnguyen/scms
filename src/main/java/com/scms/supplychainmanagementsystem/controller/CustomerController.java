70684f68979f3f792e8474bc688f859dce0a89e6
KcDih <kieudinh1999@gmail.com>
2021-10-05 13:17:28 +0700

    sửa entity part2

--- src/main/java/com/scms/supplychainmanagementsystem/controller/CustomerController.java
+++ src/main/java/com/scms/supplychainmanagementsystem/controller/CustomerController.java
@@ -4,11 +4,15 @@ import com.scms.supplychainmanagementsystem.entity.Customer;
 import com.scms.supplychainmanagementsystem.service.ICustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static org.springframework.http.ResponseEntity.status;
 
 
 @AllArgsConstructor
@@ -24,7 +28,11 @@ public class CustomerController {
 //    }
 
     @GetMapping("/list")
-    public List<Customer> getAllCustomer(){
-        return iCustomerService.getAllCustomer();
+    public ResponseEntity<List<Customer>> getAllCustomer(){
+        log.info("[Start Customer - list]");
+        List<Customer> customerslist = iCustomerService.getAllCustomer();
+        log.info("[End Customer - list]");
+        return status(HttpStatus.OK).body(customerslist);
     }
+
 }
