b490cf6a7960905b3ff8648d34dc0f1d0a47d162
KcDih <kieudinh1999@gmail.com>
2021-10-05 20:16:08 +0700

    sửa entity part2

--- src/main/java/com/scms/supplychainmanagementsystem/service/imp/CustomerServiceImpl.java
+++ src/main/java/com/scms/supplychainmanagementsystem/service/imp/CustomerServiceImpl.java
@@ -45,44 +45,44 @@ public class CustomerServiceImpl implements ICustomerService {
 
     @Override
     public void saveCustomer(CustomerDto customerDto) {
-        log.info("[Start CustomerService - saveCustomer with username: " + customerDto.get + "]");
-        if (userRepository.existsByUsername(userDto.getUsername())) {
-            throw new AppException("Username exists");
-        }
-        log.info("[Start get current user]");
-        User currentUser = userCommon.getCurrentUser();
-        log.info("[End get current user : " + currentUser.getUsername() + "]");
-        if (userDto.getRoleId() == 1) {
-            if (currentUser.getRole().getRoleID() != 1) {
-                throw new AppException("You are not allow to create role ADMIN");
-            }
-        }
-        Warehouse warehouse = new Warehouse();
-        if (currentUser.getRole().getRoleID() == 1) {
-            if (userDto.getRoleId() != 1) {
-                warehouse.setWarehouseID(userDto.getWarehouseId());
-            }
-        }
-        User user = User.builder()
-                .username(userDto.getUsername())
-                .password(passwordEncoder.encode("123@456"))
-                .email(userDto.getEmail())
-                .role(roleRepository.findById(userDto.getRoleId())
-                        .orElseThrow(() -> new AppException("Not found role")))
-                .warehouse(warehouse)
-                .firstName(userDto.getFirstName())
-                .lastName(userDto.getLastName())
-                .isActive(userDto.isActive())
-                .phone(userDto.getPhone())
-                .dateOfBirth(userDto.getDateOfBirth())
-                .district(District.builder().districtID(userDto.getDistrictId()).build())
-                .streetAddress(userDto.getStreetAddress())
-                .createdDate(Instant.now())
-                .createdBy(currentUser)
-                .build();
-        log.info("[Start save user " + user.getUsername() + " to database]");
-        userRepository.saveAndFlush(user);
-        log.info("[End save user " + user.getUsername() + " to database]");
-        log.info("[End UserService - saveUser with username: " + userDto.getUsername() + "]");
+//        log.info("[Start CustomerService - saveCustomer with username: " + customerDto.get + "]");
+//        if (userRepository.existsByUsername(userDto.getUsername())) {
+//            throw new AppException("Username exists");
+//        }
+//        log.info("[Start get current user]");
+//        User currentUser = userCommon.getCurrentUser();
+//        log.info("[End get current user : " + currentUser.getUsername() + "]");
+//        if (userDto.getRoleId() == 1) {
+//            if (currentUser.getRole().getRoleID() != 1) {
+//                throw new AppException("You are not allow to create role ADMIN");
+//            }
+//        }
+//        Warehouse warehouse = new Warehouse();
+//        if (currentUser.getRole().getRoleID() == 1) {
+//            if (userDto.getRoleId() != 1) {
+//                warehouse.setWarehouseID(userDto.getWarehouseId());
+//            }
+//        }
+//        User user = User.builder()
+//                .username(userDto.getUsername())
+//                .password(passwordEncoder.encode("123@456"))
+//                .email(userDto.getEmail())
+//                .role(roleRepository.findById(userDto.getRoleId())
+//                        .orElseThrow(() -> new AppException("Not found role")))
+//                .warehouse(warehouse)
+//                .firstName(userDto.getFirstName())
+//                .lastName(userDto.getLastName())
+//                .isActive(userDto.isActive())
+//                .phone(userDto.getPhone())
+//                .dateOfBirth(userDto.getDateOfBirth())
+//                .district(District.builder().districtID(userDto.getDistrictId()).build())
+//                .streetAddress(userDto.getStreetAddress())
+//                .createdDate(Instant.now())
+//                .createdBy(currentUser)
+//                .build();
+//        log.info("[Start save user " + user.getUsername() + " to database]");
+//        userRepository.saveAndFlush(user);
+//        log.info("[End save user " + user.getUsername() + " to database]");
+//        log.info("[End UserService - saveUser with username: " + userDto.getUsername() + "]");
     }
 }
