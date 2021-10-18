b0f6d74677d2f5bbaca988b0c9473621dce41164
Thuy Quynh <ptq1202@gmail.com>
2021-10-18 22:22:31 +0700

    Revert "update config spring"
    This reverts commit 567797d618a0be7a7ebb9a09bd11c332202eb1fa.

--- src/main/java/com/scms/supplychainmanagementsystem/SupplyChainManagementSystemApplication.java
+++ src/main/java/com/scms/supplychainmanagementsystem/SupplyChainManagementSystemApplication.java
@@ -2,16 +2,9 @@ package com.scms.supplychainmanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SupplyChainManagementSystemApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SupplyChainManagementSystemApplication.class);
-    }
+public class SupplyChainManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SupplyChainManagementSystemApplication.class, args);
