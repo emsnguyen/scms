e6f546b9a04597e9f6f3812fd52491aeb4e5b645
Thuy Quynh <ptq1202@gmail.com>
2021-10-25 00:09:48 +0700

    add OrderDetails p1

--- src/main/java/com/scms/supplychainmanagementsystem/repository/StockRepository.java
+++ src/main/java/com/scms/supplychainmanagementsystem/repository/StockRepository.java
@@ -1,10 +1,16 @@
 package com.scms.supplychainmanagementsystem.repository;
 
+import com.scms.supplychainmanagementsystem.entity.Product;
 import com.scms.supplychainmanagementsystem.entity.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
+    @Query(value = "select case when s.availableQuantity > 0 then true else false end" +
+            " from Stock s where s.product.productId = :productId")
+    boolean checkQtyAvailablePositive(Long productId);
 
+    Stock findByProduct(Product product);
 }
