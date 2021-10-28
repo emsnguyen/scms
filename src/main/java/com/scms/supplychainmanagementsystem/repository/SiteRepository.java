0fe93a3e40b343e449b507b7f34bcaee24d5ec07
KcDih <kieudinh1999@gmail.com>
2021-10-28 21:52:34 +0700

    Site CRUD

--- src/main/java/com/scms/supplychainmanagementsystem/repository/SiteRepository.java
+++ src/main/java/com/scms/supplychainmanagementsystem/repository/SiteRepository.java
@@ -2,8 +2,10 @@ package com.scms.supplychainmanagementsystem.repository;
 
 import com.scms.supplychainmanagementsystem.entity.PurchaseDetails;
 import com.scms.supplychainmanagementsystem.entity.Site;
+import com.scms.supplychainmanagementsystem.entity.Stock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,29 +14,20 @@ import org.springframework.stereotype.Repository;
 import javax.transaction.Transactional;
 
 @Repository
-public interface SiteRepository {
+public interface SiteRepository extends JpaRepository<Site, Long> {
 
-    @Query(value = "SELECT u FROM PurchaseDetails u where u.purchaseDetailID= :purchasedetailid  and u.purchase.warehouse= :warehouseid")
-    PurchaseDetails findByPurchaseDetailId(@Param("purchasedetailid") Long purchaseDetailid, @Param("warehouseid") Long warehouseid);
-
-    @Query(value = "SELECT u FROM PurchaseDetails u where u.purchaseDetailID= :purchasedetailid ")
-    PurchaseDetails findByPurchaseDetailIdAdmin(@Param("purchasedetailid") Long purchasedetailid);
-
+    @Query(value = "SELECT u FROM Site u where u.siteId= :siteId  and u.warehouse.warehouseID= :warehouseid")
+    Site findBySiteId(@Param("siteId") Long siteId, @Param("warehouseid") Long warehouseid);
 
     @Modifying
     @Transactional
-    @Query(value = "Delete  FROM PurchaseDetails u where u.purchaseDetailID= :purchasedetailid  and u.purchase.warehouse= :warehouseid")
-    void deletePurchaseDetail(@Param("purchasedetailid") Long purchasedetailid, @Param("warehouseid") Long warehouseid);
+    @Query(value = "Delete  FROM Site u where u.siteId= :siteId and u.warehouse.warehouseID =:warehouseId  ")
+    void deleteSite(@Param("siteId") Long siteId,@Param("warehouseId") Long warehouseId);
 
     @Modifying
     @Transactional
-    @Query(value = "Delete  FROM PurchaseDetails u where u.purchase.purchaseID= :purchaseid  ")
-    void deletePurchaseDetailByPurchaseId(@Param("purchaseid") Long purchaseid);
-
-    @Modifying
-    @Transactional
-    @Query(value = "Delete  FROM PurchaseDetails u where u.purchaseDetailID= :purchasedetailid ")
-    void deletePurchaseAdmin(@Param("purchasedetailid") Long purchasedetailid);
+    @Query(value = "Delete  FROM Site u where u.siteId= :siteId ")
+    void deleteSiteAdmin(@Param("siteId") Long siteId);
 
     @Query(value = "select u from Site u where u.warehouse.warehouseID=:warehouseId  ")
     Page<Site> filterInOneWarehouse(@Param("warehouseId") Long warehouseId, Pageable pageable);
